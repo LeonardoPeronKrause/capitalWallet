75e2e162e953caf79377fd17f564b0e9a311b066
LeonardoPeronKrause <lperonkrause@gmail.com>
2025-04-19 00:22:30 -0300

    Updating transactions table

--- backend/src/database/ddl.sql
+++ backend/src/database/ddl.sql
@@ -5,11 +5,15 @@ CREATE TYPE transaction_type AS ENUM ('receita', 'despesa', 'investimento');
 
 -- Transactions table
 CREATE TABLE transactions (
-	id SERIAL PRIMARY KEY,
-	type transaction_type NOT NULL,
-	description TEXT,
-	value NUMERIC(10,2) NOT NULL,
-	date DATE DEFAULT CURRENT_DATE
+    id SERIAL PRIMARY KEY,
+    type transaction_type NOT NULL,
+    description TEXT,
+    value NUMERIC(10,2) NOT NULL,
+    date DATE DEFAULT CURRENT_DATE,
+    user_id INTEGER NOT NULL, -- This column stores the ID of the user who ows the transaction (required)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE 
+	-- This sets a foreign key constraint to ensure user_id exists in the users table (referring to the id column)
+    -- ON DELETE CASCADE: if a user is deleted, all their related transactions will also be automatically deleted
 );
 
 -- Users table, to have multiple regiters
